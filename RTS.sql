27d4222d1acf243c8a1f708db4c64bf1066fa354
SzYmoonJ <kaliozajankowski@gmail.com>
2022-09-28 15:29:19 +0200

    plik eksport

--- RTS.sql
+++ RTS.sql
@@ -1,7 +1,7 @@
 		#players
-INSERT INTO players (nick, email, password) VALUES (Kflek, mail@gmail.com, hasło123)
-INSERT INTO players (nick, email, password) VALUES (TCZ, Tcz@gmail.com, masło123)
-INSERT INTO players (nick, email, password) VALUES (Reg, tre@gmail.com, hsl123)
+INSERT INTO players (nick, email, password) VALUES ("Kflek", "mail@gmail.com", "hasło123")
+INSERT INTO players (nick, email, password) VALUES ("TCZ", "Tcz@gmail.com", "masło123")
+INSERT INTO players (nick, email, password) VALUES ("Reg", "tre@gmail.com", "hsl123")
 
 
 		#villages
@@ -11,22 +11,22 @@ INSERT INTO villages (name, x, y, players_id) VALUES ("wjoha", 15, 30, 3)
 
 
 		#troops
-INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES łucznik, no ma łuk, 0, 10, 4)
-INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES (miecznik, macha mieczem, 8, 0, 8)
-INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES (kusznik, taki trochę lepszy łucznik, 0, 15, 8)
-INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES (ryczerz, lepszego niż on nie znajdziesz, 20, 0, 12)
+INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES ("łucznik", "no ma łuk", 0, 10, 4)
+INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES ("miecznik", "macha mieczem", 8, 0, 8)
+INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES ("kusznik", "taki trochę lepszy łucznik", 0, 15, 8)
+INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES ("ryczerz", "lepszego niż on nie znajdziesz", 20, 0, 12)
 
 
 		#resources
-INSERT INTO resources (name, descripion) VALUES (drewno, Podstawowy materiał budowlany)
-INSERT INTO resources (name, descripion) VALUES (glina, Z tego lepisz garnki... ale nie w tej grze)
-INSERT INTO resources (name, descripion) VALUES (żelazo, Do trenowania jednostek, złota nie masz to chociaż żelazo dostaną)
+INSERT INTO resources (name, description) VALUES ("Drewno", "Podstawowy materiał budowlany")
+INSERT INTO resources (name, description) VALUES ("Glina", "Z tego lepisz garnki... ale nie w tej grze")
+INSERT INTO resources (name, description) VALUES ("Żelazo", "Do trenowania jednostek, złota nie masz to chociaż żelazo dostaną")
 
 
 		#buildings
-INSERT INTO buildings (name) VALUES (dół z gliną)
-INSERT INTO buildings (name) VALUES (huta żelaza)
-INSERT INTO buildings (name) VALUES (chata drwala)
+INSERT INTO buildings (name) VALUES ("Dół z gliną")
+INSERT INTO buildings (name) VALUES ("Huta żelaza")
+INSERT INTO buildings (name) VALUES ("Chata drwala")
 
 
 		#login
