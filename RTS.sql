124ebcf0dda4131c684676c372ee2baad1673d06
SzYmoonJ <kaliozajankowski@gmail.com>
2022-09-28 15:50:20 +0200

    dodane surowce

--- RTS.sql
+++ RTS.sql
@@ -8,7 +8,19 @@ INSERT INTO players (nick, email, password) VALUES ("Reg", "tre@gmail.com", "hsl
 INSERT INTO villages (name, x, y, players_id) VALUES ("wiocha", 10, 20, 2)
 INSERT INTO villages (name, x, y, players_id) VALUES ("wjocha", 11, 24, 1)
 INSERT INTO villages (name, x, y, players_id) VALUES ("wjoha", 15, 30, 3)
+	
+		#villages-resources
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (1, 1, 1000)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (1, 2, 1200)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (1, 3, 1500)
 
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (2, 1, 1001)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (2, 2, 1201)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (2, 3, 1501)
+
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (3, 1, 1002)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (3, 2, 1202)
+INSERT INTO villages_has_resources (villages_id, resources_id, value) VALUES (3, 3, 1502)
 
 		#troops
 INSERT INTO troops (name, description, damage, distance_damage, defense) VALUES ("łucznik", "no ma łuk", 0, 10, 4)
